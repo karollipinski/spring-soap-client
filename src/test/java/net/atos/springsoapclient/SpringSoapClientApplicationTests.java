43df0c9b004b846397c8db5fa789b46b2d834cf1
klipinski <karol.lipinski@fenige.pl>
2019-12-04 09:15:54 +0100

    Init

---
+++ src/test/java/net/atos/springsoapclient/SpringSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.springsoapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
